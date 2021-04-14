aaa3a84733225f8759ff74eb66159da3a3c5312f
sapatitoscripts <octaviocu2020@gmail.com>
2021-04-14 10:41:26 -0300

    Added some reanims.

---
+++ ReanimsScript/NullWareReanimR6.lua
@@ -0,0 +1,6 @@
+--Your limbs may fall off beacause of the net patch made by roblox. 
+--Good reanim, requires netOwnership, and it doesnt jitter.
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/fc29cbf0eda6f8b129778b441be3128f/raw/6StQ2n56PnEHMhQ9"))()
+
+--I DIDN´T MADE THIS REANIM SCRIPT.
---
+++ ReanimsScript/SelexityReanimR6.lua
@@ -0,0 +1,5 @@
+--Selexity reanims, netless and R6, has no dummy.
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SynHaru/Selexity/main/Reanimate'),true))()
+
+--I DIDN´T MADE THIS REANIM SCRIPT.
