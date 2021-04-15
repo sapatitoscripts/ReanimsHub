1baf9c828543f788bd74b122e25fd0d56df288ef
sapatitoscripts <octaviocu2020@gmail.com>
2021-04-15 01:19:01 -0300

    Added a Unrecognizable script, may not work on all the a games

---
+++ OtherScripts/DeleteNameTagAndBeUnrecognizable.lua
@@ -0,0 +1,35 @@
+local plr = game.Players.LocalPlayer
+local chr = plr.Character
+--//Just a branch of the brazilian script//--
+
+--//BeUnrecognizable//-- (Usefull to cheat without being banned.)
+for i, v in pairs(chr:GetChildren()) do
+
+    --//DeleteNametag//--
+    if v:IsA("BillboardGui") then
+        v:Destroy()
+    end
+    --//DeleteNametagEnd//--
+    
+    --//DeleteFace//--
+    if v.Name == "Head" then
+        v.face:Destroy()
+    end
+    --//DeleteFaceEnd//--
+    
+    --//RemoveShirtAndPants//--
+    if v.Name == "Shirt" then
+        v:Destroy()
+    end
+    if v.Name == "Pants" then
+        v:Destroy()
+    end
+    --//RemoveShirtAndPantsEnd//--
+    
+    --//AccessoryDestroy//--
+    if v:IsA("Accessory") then
+        v:Destroy()
+    end
+    --//AccessoryDestroyEnd//--
+    end
+    --//BeUnrecognizableEnd//--
